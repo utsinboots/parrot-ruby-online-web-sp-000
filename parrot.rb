1a00886f07060cf98038ff71431c6e496c9c6821
Utshant Gurung <utshantgurung@gmail.com>
2021-05-20 10:31:33 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,13 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-/begin
+=begin
 def parrot(i="Squawk!")
   puts i
   return i
 end
 parrot()
-/end
+=end
+def parrot(i="Squawk!")
+  puts i
+  i
+end
